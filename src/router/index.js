e7698651a69285139ccdb2926dd553d8930890ff
NuongBee2003 <thanhdats22003@gmail.com>
2025-04-24 20:03:16 +0700

    fix: hash routing + postMessage JSON

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,16 @@ const routes = [
   { path: '/about', name: 'About', component: About },
 ]
 
-// const router = createRouter({
-//   history: createWebHashHistory(),  // ← CHÚ Ý: hash history
-//   routes,
-// })
-
-// export default router
 const router = createRouter({
-      // chú ý import.meta.env.BASE_URL sẽ tương ứng với `base` trong vite.config.js
-     history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),  // ← CHÚ Ý: hash history
   routes,
-    })
-    
+})
+
 export default router
+// const router = createRouter({
+//       // chú ý import.meta.env.BASE_URL sẽ tương ứng với `base` trong vite.config.js
+//      history: createWebHistory(import.meta.env.BASE_URL),
+//   routes,
+//     })
+    
+// export default router
